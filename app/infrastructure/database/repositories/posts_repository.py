80d1a7a9dab052daf1edc573711a220f548000c1
Oliver Arthur <contact@oliverarthurnardi.com>
2021-11-23 22:12:56 +0100

    cleaning code for posts module

--- app/infrastructure/database/repositories/posts_repository.py
+++ app/infrastructure/database/repositories/posts_repository.py
@@ -1,3 +1,6 @@
+from __future__ import annotations
+
+from fastapi import HTTPException, status
 from sqlalchemy.sql import func
 
 from app.domain.posts.entities.posts import Posts
@@ -6,18 +9,25 @@ from app.infrastructure.database.models.vote import Vote as VoteModel
 from app.infrastructure.database.sqlalchemy import db
 
 
-def fetch(id: int) -> Posts:
-    return (
+def fetch(post_id: int) -> Posts:
+    post = (
         db.query(PostsModel, func.count(VoteModel.post_id).label("votes"))
         .join(VoteModel, VoteModel.post_id == PostsModel.id, isouter=True)
         .group_by(PostsModel.id)
-        .filter(PostsModel.id == id)
+        .filter(PostsModel.id == post_id)
         .first()
     )
 
+    if post is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Post not found",
+        )
+    return post
+
 
 def search(page: int, per_page: int, search: str = "") -> list:
-    return (
+    posts = (
         db.query(PostsModel, func.count(VoteModel.post_id).label("votes"))
         .join(VoteModel, VoteModel.post_id == PostsModel.id, isouter=True)
         .group_by(PostsModel.id)
@@ -27,11 +37,20 @@ def search(page: int, per_page: int, search: str = "") -> list:
         .all()
     )
 
+    if posts is None or len(posts) == 0:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="No entries found with these parameters",
+        )
+    return posts
+
 
 def create(data: Posts, user_id: int) -> Posts:
-
     if len(data.title) == 0 or len(data.content) == 0:
-        return None
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Title and content fields can not be empty.",
+        )
 
     values = data.__dict__
     posts = PostsModel(owner_id=user_id, **values)
@@ -43,11 +62,19 @@ def create(data: Posts, user_id: int) -> Posts:
     return posts
 
 
-def update(posts_id: int, values: dict, user_id: int) -> Posts:
-    posts = fetch(posts_id)
+def update(posts_id: int, values: dict, user_id: int) -> Posts | None:
+    posts = db.query(PostsModel).filter(PostsModel.id == posts_id).first()
 
-    if posts is None or posts.owner_id != user_id:
-        return None
+    if posts is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Post not found"
+        )
+
+    if posts.owner_id != user_id:
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail="Your are not allowed to update this post",
+        )
 
     for key, value in values:
         setattr(posts, key, value)
@@ -59,10 +86,19 @@ def update(posts_id: int, values: dict, user_id: int) -> Posts:
 
 
 def published(posts_id: int, user_id: int) -> bool:
-    posts = fetch(posts_id)
+    posts = db.query(PostsModel).filter(PostsModel.id == posts_id).first()
 
-    if posts is None or posts.owner_id != user_id:
-        return False
+    if posts is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Post not found",
+        )
+
+    if posts.owner_id != user_id:
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail="You are not allowed to publish this post",
+        )
 
     posts.published = True
     db.commit()
@@ -71,24 +107,45 @@ def published(posts_id: int, user_id: int) -> bool:
     return True
 
 
-def unpublish(posts_id: int, user_id: int) -> bool:
-    post = fetch(posts_id)
+def unpublished(posts_id: int, user_id: int) -> bool:
+    posts = db.query(PostsModel).filter(PostsModel.id == posts_id).first()
 
-    if post is None or post.owner_id != user_id:
-        return False
+    if posts is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Post not found",
+        )
 
-    post.published = False
+    if posts.owner_id != user_id:
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail="You are not allowed to unpublished this post",
+        )
+
+    posts.published = False
     db.commit()
-    db.refresh(post)
+    db.refresh(posts)
 
     return True
 
 
 def delete(posts_id: int, user_id: int) -> bool:
-    post = fetch(posts_id)
+    posts = db.query(PostsModel).filter(PostsModel.id == posts_id).first()
 
-    if post is None or post.owner_id != user_id:
+    if posts is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Post not found",
+        )
+
+    if posts.owner_id != user_id:
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail="You are not allowed to delete this post",
+        )
+
+    if posts is None or posts.owner_id != user_id:
         return False
 
-    db.delete(post)
+    db.delete(posts)
     db.commit()
