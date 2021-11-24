83073e9c24cc880c4c2a2df27ef2fba7ce1a4645
Oliver Arthur <contact@oliverarthurnardi.com>
2021-11-24 10:58:51 +0100

    create comments model

--- app/infrastructure/database/base.py
+++ app/infrastructure/database/base.py
@@ -1,4 +1,5 @@
 from app.infrastructure.database.base_class import Base  # noqa
+from app.infrastructure.database.models.comments import Comments  # noqa
 from app.infrastructure.database.models.posts import Posts  # noqa
 from app.infrastructure.database.models.user import User  # noqa
 from app.infrastructure.database.models.vote import Vote  # noqa
---
+++ app/infrastructure/database/models/comments.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, DateTime, ForeignKey, Integer, String
+from sqlalchemy.sql import func
+
+from app.infrastructure.database.base_class import Base
+
+
+class Comments(Base):
+    __tablename__ = "comments"
+
+    id = Column(Integer, primary_key=True)
+    body = Column(String(250), nullable=False)
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    post_id = Column(Integer, ForeignKey("posts.id"), nullable=False)
+    created_at = Column(DateTime(timezone=True), nullable=False, default=func.now())
+    updated_at = Column(DateTime(timezone=True), nullable=False, onupdate=func.now())
